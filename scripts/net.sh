8032d56c8d19c3a4ea52d9463b32efb243255c1b
0xnofoot <x0xnofoot@163.com>
2024-01-22 00:22:35 +0800

    update net scripts

--- scripts/net.sh
+++ scripts/net.sh
@@ -9,7 +9,7 @@ get_network_stat_download() {
     interface_rx=$(echo "$ifstat_output" | awk -v key="$interface" '$0 ~ "^" key {print $6}')
 
     if [[ $interface_rx =~ ^([0-9]+)$ ]]; then
-        interface_rx="$(echo "scale=1; $interface_rx / 1024" | bc)K"
+        interface_rx="$(echo "scale=0; $interface_rx / 1024" | bc)K"
     elif [[ $interface_rx =~ ^([0-9]+)K$ ]]; then
         number_part="${BASH_REMATCH[1]}"
         interface_rx="$(echo "scale=1; $number_part / 1024" | bc)M"
@@ -22,10 +22,11 @@ get_network_stat_upload() {
     interface_tx=$(echo "$ifstat_output" | awk -v key="$interface" '$0 ~ "^" key {print $8}')
 
     if [[ $interface_tx =~ ^([0-9]+)$ ]]; then
-        interface_tx="$(echo "scale=1; $interface_tx / 1024" | bc)K"
+        interface_tx="$(echo "scale=0; $interface_tx / 1024" | bc)K"
     elif [[ $interface_rx =~ ^([0-9]+)K$ ]]; then
         number_part="${BASH_REMATCH[1]}"
         interface_tx="$(echo "scale=1; $number_part / 1024" | bc)M"
+
     fi
 
     echo "$interface_tx"
